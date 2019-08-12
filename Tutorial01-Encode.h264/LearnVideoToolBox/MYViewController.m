ce6d98a28076b8b9177bd5b99d987d116e15e495
Len <787118083@qq.com>
2019-08-12 10:48:18 +0800

    测试

---
+++ Tutorial01-Encode.h264/LearnVideoToolBox/MYViewController.m
@@ -0,0 +1,32 @@
+//
+//  MYViewController.m
+//  LearnVideoToolBox
+//
+//  Created by dev on 2019/8/12.
+//  Copyright © 2019 林伟池. All rights reserved.
+//
+
+#import "MYViewController.h"
+
+@interface MYViewController ()
+
+@end
+
+@implementation MYViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
